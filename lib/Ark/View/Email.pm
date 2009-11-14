c6c5dcb532ac26e7e40850fe21bd9b7b5678efa6
haoyayoi <haoyayoi@d0d07461-0603-4401-acd4-de1884942a52>
2009-11-14 11:51:23 +0000

    Initial commit
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Ark-View-Email/trunk@35903 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Ark/View/Email.pm
@@ -0,0 +1,32 @@
+package Ark::View::Email;
+
+use Ark 'View';
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Ark::View::Email -
+
+=head1 SYNOPSIS
+
+  use Ark::View::Email;
+
+=head1 DESCRIPTION
+
+Ark::View::Email is
+
+=head1 AUTHOR
+
+haoyayoi E<lt>st.hao.yayoi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
